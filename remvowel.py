cf2ae80bc676b36d3ebc7208058e04891f2e53ea
Deadly_Cabbage <31901047+JasonPapas@users.noreply.github.com>
2017-09-15 00:14:23 +0300

    Update remvowel.py

--- remvowel.py
+++ remvowel.py
@@ -4,7 +4,7 @@ def disemvowel(string):
     print("string={0}  list={1}".format(len(string), len(string_)))
     i = 0
     while i < len(string):
-        if string_[i] == 'e':
+        if string_[i] == ('e' or "a" or "i" or "o" or "u" or "y":
             string_.remove(string_[i])
             print("{0}".format(i))
         i += 1
