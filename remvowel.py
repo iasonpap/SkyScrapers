fdcc13e03095a66ff86f015307b322fa4c97e529
Deadly_Cabbage <31901047+JasonPapas@users.noreply.github.com>
2017-09-14 23:58:16 +0300

    Add files via upload

---
+++ remvowel.py
@@ -0,0 +1,16 @@
+def disemvowel(string):
+    string_ = list(string)
+    print(string_)
+    print("string={0}  list={1}".format(len(string), len(string_)))
+    i = 0
+    while i < len(string):
+        if string_[i] == 'e':
+            string_.remove(string_[i])
+            print("{0}".format(i))
+        i += 1
+    print(string_)
+    new_str = ''.join(string_)
+    return new_str
+
+
+print(disemvowel("Hello World!"))
